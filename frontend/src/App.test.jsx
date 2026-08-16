93ce40bea33d0ff5821336f4ea271b318fb92035
Dinuka <dinukaniroshan0210@gmail.com>
2026-08-16 23:08:15 +0530

    test: fix App.test.jsx and mock localStorage in setupTests.js

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,14 +1,9 @@
 import { render } from '@testing-library/react';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 describe('App Component', () => {
   it('renders without crashing', () => {
     // This is a simple smoke test
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    render(<App />);
   });
 });
